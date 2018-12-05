fb6eff9858e6d353bdb166f658aafcf7c10c4ab3
luojun <luojun@ssitcloud.com>
2018-12-05 16:33:28 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
 </template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,15 +17,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  margin-top: 200px;
+
+  h1 {
+    color: #42b983;
   }
 }
 </style>
